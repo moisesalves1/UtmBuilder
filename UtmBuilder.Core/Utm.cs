720b5d9c2bc15600d25a86994ed7c8c11a2f8d53
Moisés Alves <moisesalves1@gmail.com>
2023-09-19 06:58:11 -0300

    CoreClass, ValueObjects and Exceptions

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,21 @@
+﻿using UtmBuilder.Core.ValueObjtecs;
+
+namespace UtmBuilder.Core;
+
+public class Utm
+{
+    public Utm(Url url, Campaign campaign)
+    {
+        Url = url;
+        Campaign = campaign;
+    }
+
+    /// <summary>
+    /// URL (WebSite Link)
+    /// </summary>
+    public Url Url { get; }
+    /// <summary>
+    /// Campaign Details
+    /// </summary>
+    public Campaign Campaign { get; }
+}
---
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.ValueObjtecs;
+
+public class Url : ValueObject
+{
+    /// <summary>
+    /// Create a new URL
+    /// </summary>
+    /// <param name="address">Address of URL (WebSite Link)</param>
+    public Url(string address)
+    {
+        Address = address;
+        InvalidUrlException.ThrowIfInvalid(address);
+    }
+    /// <summary>
+    ///  Address of URL (WebSite Link)
+    /// </summary>
+    public string Address { get; }
+}
